2524edfaac32ac101dfce9e4e197774660fa1eb2
Andrew Li <andrewli6504@gmail.com>
2026-05-09 17:25:10 -0500

    more wip

--- assets/scripts/buyable.gd
+++ assets/scripts/buyable.gd
@@ -20,3 +20,14 @@ func unhighlight():
 
 func interact():
 	super.interact()
+	# Check if player has enough money to buy
+	var canBuy = true
+
+	if canBuy:
+		# Play sound effect for buying
+		# Deduct price from player wallet
+		pass
+	else:
+		# Play sound effect for not being able to afford
+		# Flash red?
+		pass
