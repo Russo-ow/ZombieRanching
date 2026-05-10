af1dfa2a798815a3df80da6ee825fac3c92a2bac
Andrew Li <andrewli6504@gmail.com>
2026-05-10 14:33:14 -0500

    shop items assigned in editor instead of hardcoded

--- assets/scripts/shop.gd
+++ assets/scripts/shop.gd
@@ -1,23 +1,11 @@
 extends Node
 
-var shop_items: Array[Buyable]
+@export var shop_items: Array[Buyable]
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	# Hardcoding for now
-	var item = $ShopItems/Gun as Buyable
-	item.buySuccess.connect(purchase)
-	item.buyFail.connect(purchase_fail)
-	shop_items.append(item)
-	item = $ShopItems/Ammo as Buyable
-	item.buySuccess.connect(purchase)
-	item.buyFail.connect(purchase_fail)
-	shop_items.append(item)
-	item = $ShopItems/Health as Buyable
-	item.buySuccess.connect(purchase)
-	item.buyFail.connect(purchase_fail)
-	shop_items.append(item)
-
+	for item: Buyable in shop_items:
+		item.buySuccess.connect(purchase)
+		item.buyFail.connect(purchase_fail)
 
 func purchase():
 	$PurchaseAudio.play()
