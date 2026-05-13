839537f5bfa8dad5d1b40c47d49d806697e0282d
Russo-ow <e10benga@gmail.com>
2026-05-12 20:00:52 -0400

    ammo implemented

--- assets/scripts/gun.gd
+++ assets/scripts/gun.gd
@@ -6,6 +6,9 @@ extends Node
 @export var sprite_reload : Texture2D
 
 @export_category("Data")
+@export var max_ammo := 99
+@export var clip_size := 6
+@export_group("Timing")
 @export var firing_cooldown := .3
 @export var firing_frame_duraction := .2
 @export var reload_duration := 2.
@@ -14,15 +17,21 @@ var sprite_node : Sprite3D
 var raycast : RayCast3D
 var audio_fire : AudioStreamPlayer3D
 var audio_reload : AudioStreamPlayer3D
+var audio_empty : AudioStreamPlayer3D
 
 var is_reloading : bool
 var stopwatch : float
+var clip : int
+var ammo : int
 
 func _ready():
 	sprite_node = $RevolverSprite
 	raycast = $WeaponRaycast
 	audio_fire = $FireAudio
 	audio_reload = $ReloadAudio
+	audio_empty = $EmptyAudio
+	ammo = max_ammo
+	clip = clip_size
 
 func _process(delta: float):
 	if is_reloading || stopwatch < firing_cooldown:
@@ -37,6 +46,8 @@ func _process(delta: float):
 		fire()
 
 func start_reload():
+	if clip == clip_size:
+		return
 	is_reloading = true
 	stopwatch = 0
 	sprite_node.texture = sprite_reload
@@ -46,8 +57,16 @@ func end_reload():
 	is_reloading = false
 	stopwatch = firing_cooldown
 	sprite_node.texture = sprite_default
+	var to_add = min(clip_size - clip, ammo)
+	ammo -= to_add
+	clip += to_add
 
 func fire():
+	print(clip)
 	stopwatch = 0
+	if clip == 0:
+		audio_empty.play()
+		return
 	sprite_node.texture = sprite_firing
+	clip -= 1
 	audio_fire.play()
